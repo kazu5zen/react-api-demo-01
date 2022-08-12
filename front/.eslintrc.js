3860ad5b96a67e6b28bafc97287d32fdbac38ab3
kazu5zen <kazuma.ryogetsu5@gmail.com>
2022-08-12 14:07:43 +0900

    prettierでfix

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -2,40 +2,32 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint", "jest", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'jest', 'prettier'],
   rules: {
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
-    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
-    "import/extensions": [
-      "error",
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-    ],
-    "prettier/prettier": "error",
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'import/extensions': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'prettier/prettier': 'error',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
