3e8aedde6e63fd33e5b621cac7f9e746d468fe2c
kazu5zen <kazuma.ryogetsu5@gmail.com>
2022-09-19 08:55:13 +0900

    グラフ表示画面の都道府県選択実装 Fixes #5 ・都道府県の選択チェックボックスでsetStateを使うように変更 ・リファクタリング：ファイル分割、ディレクトリ名変更

--- front/src/components/pages/GraphDisplay.tsx
+++ front/src/components/pages/GraphDisplay.tsx
@@ -1,36 +1,18 @@
 import { FC, memo } from 'react';
-import { Link } from 'react-router-dom';
-import PrefecturesCheckBox from '../ui-elements/CheckBox/PrefecturesCheckBox';
 import Footer from '../ui-parts/Footer/Footer';
 import Header from '../ui-parts/Header/Header';
+import GraphDisplayPanel from '../ui-parts/Panel/GraphDisplayPanel';
 
-export const Login: FC = memo(() => {
-  const prefecturesList = [
-    { id: '1', name: '東京都' },
-    { id: '2', name: '千葉県' },
-    { id: '3', name: '埼玉県' },
-    { id: '4', name: '神奈川県' },
-    { id: '5', name: '群馬県' },
-  ];
-
+export const GraphDisplay: FC = memo(() => {
   return (
     <>
       <Header />
       <main>
-        <h4>都道府県</h4>
-        <div>
-          {prefecturesList.map((value) => (
-            <PrefecturesCheckBox id={value.id} text={value.name} />
-          ))}
-        </div>
-        <div>グラフ表示コンポーネントを追加予定</div>
-        <div>
-          <Link to="/">トップページへ戻る</Link>
-        </div>
+        <GraphDisplayPanel />
       </main>
       <Footer />
     </>
   );
 });
 
-export default Login;
+export default GraphDisplay;
--- front/src/components/pages/Login.tsx
+++ front/src/components/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { FC, memo } from 'react';
 import Footer from '../ui-parts/Footer/Footer';
 import Header from '../ui-parts/Header/Header';
-import LoginPanel from '../ui-parts/LoginPanel/LoginPanel';
+import LoginPanel from '../ui-parts/Panel/LoginPanel';
 
 export const Login: FC = memo(() => {
   return (
