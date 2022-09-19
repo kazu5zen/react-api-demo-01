682e911fe10e49939593d14972c261b84c9dfcc4
kazu5zen <kazuma.ryogetsu5@gmail.com>
2022-09-19 16:54:09 +0900

    グラフ表示画面のグラフ部分の実装 Fixes #6 ・GoogleChartの表示を実装（データは仮）

--- front/src/components/ui-parts/Panel/GraphDisplayPanel.tsx
+++ front/src/components/ui-parts/Panel/GraphDisplayPanel.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, FC, memo, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Chart } from 'react-google-charts';
 import PrefecturesCheckBox from '../../ui-elements/CheckBox/PrefecturesCheckBox';
 
 type Prefectures = {
@@ -26,6 +27,34 @@ export const GraphDisplayPanel: FC = memo(() => {
     );
   };
 
+  const chartData = [
+    [{ type: 'date', label: '年度' }, '東京都', '千葉県'],
+    [new Date(2011, 1), 19000, 900],
+    [new Date(2012, 1), 20000, 1000],
+    [new Date(2013, 1), 31000, 1100],
+    [new Date(2014, 1), 22000, 1200],
+    [new Date(2015, 1), 23000, 1300],
+    [new Date(2016, 1), 24000, 1400],
+    [new Date(2017, 1), 35000, 1500],
+    [new Date(2018, 1), 26000, 1600],
+    [new Date(2019, 1), 27000, 1700],
+    [new Date(2020, 1), 28000, 1800],
+    [new Date(2021, 1), 29000, 1900],
+    [new Date(2022, 1), 30000, 2000],
+  ];
+
+  const chartOptions = {
+    chart: {
+      title: '総人口推移グラフ',
+    },
+    hAxis: {
+      title: '年度',
+    },
+    vAxis: {
+      title: '人口数',
+    },
+  };
+
   return (
     <>
       <h4>都道府県</h4>
@@ -40,7 +69,9 @@ export const GraphDisplayPanel: FC = memo(() => {
           />
         ))}
       </div>
-      <div>グラフ表示コンポーネントを追加予定</div>
+      <div>
+        <Chart chartType="LineChart" width="80%" data={chartData} options={chartOptions} />
+      </div>
       <div>
         <Link to="/">トップページへ戻る</Link>
       </div>
